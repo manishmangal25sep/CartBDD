1f011efe40d555ab339797eb90e029f3ec17002e
manishmangal25sep <78806870+manishmangal25sep@users.noreply.github.com>
2022-04-24 23:34:43 +0800

    Update testRunners.java

--- CartBDD/src/test/java/stepDefinition/testRunners.java
+++ CartBDD/src/test/java/stepDefinition/testRunners.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\masin\\eclipse-workspace\\CartBDD\\src\\test\\resources\\Features",glue= {"stepDefinition"})
+@CucumberOptions(features="C:\\Users\\masin\\eclipse-workspace\\CartBDD\\src\\test\\resources\\Features",glue= {"stepDefinition"},plugin = { "pretty", "html:target/cucumber-reports" },
+monochrome = true)
 public class testRunners {
 
 }
